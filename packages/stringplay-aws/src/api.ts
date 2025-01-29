d3c5578f7206ff23744f384e0e87910104de5987
cloudydaiyz <kyland03.biz@gmail.com>
2025-01-29 05:11:21 -0600

    api event body

--- packages/stringplay-aws/src/api.ts
+++ packages/stringplay-aws/src/api.ts
@@ -2,6 +2,7 @@ import controllers from "@cloudydaiyz/stringplay-core";
 import { APIGatewayProxyHandler } from "aws-lambda";
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-    const res = await controllers.apiController(event.path, event.httpMethod as any, event.headers, {});
+    const body = event.body ? JSON.parse(event.body) : {};
+    const res = await controllers.apiController(event.path, event.httpMethod as any, event.headers, body);
     return { statusCode: res.status, headers: res.headers as any, body: JSON.stringify(res.body) };
 }
